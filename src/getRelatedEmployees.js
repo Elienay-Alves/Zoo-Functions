90a87194bef1222bdf09597a938726328abe0af7
Elienay-Alves <elienay8486@gmail.com>
2022-01-27 18:54:39 -0300

    Requisito 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+// .some retorna true se ao menos um valor for verdadeiro
+// .includes determina se um array contém um determinado elemento, retornando true ou false apropriadamente.
+
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((personId) => personId.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((person) => person.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
