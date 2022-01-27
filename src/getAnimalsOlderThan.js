12ed9b692ee54c1fd4510398fb71f211e575cfd6
Elienay-Alves <elienay8486@gmail.com>
2022-01-27 16:43:29 -0300

    Requisito 2 refatorado e ambos comentados

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
+// Esta função, a partir do nome de uma espécie e uma idade mínima, verifica se todos os animais daquela espécie possuem a idade mínima especificada.
 
-function getAnimalsOlderThan(animal, age) {
-  return data.species.find((animale) => animale.name === animal)
-    .residents.every((resident) => resident.age > age);
-}
+// Essa função encontra com .find os animais que tem o nome da espécie igual ao passado no parametro e mostra com o .every todos os que tem o valor age maior que o passado no parametro.
+
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((animale) => animale.name === animal).residents.every((resident) => resident.age > age);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
+
+// Fizemos uma função que retorna um array contendo o nome da especie referente ao id inserido no parametro.
+// .map () retorna um array contendo as especies selecionadas pelo .find() que analisa se a id passada é igual ao valor contido na chave id.
 const getSpeciesByIds = (...ids) => ids.map((id) => data.species
-  .find((species) => species.id === id));
+  .find((specie) => specie.id === id));
 
 module.exports = getSpeciesByIds;
