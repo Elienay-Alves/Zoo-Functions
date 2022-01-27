d4ae7251cf3c7698e28467b80d548a36135d7f79
Elienay-Alves <elienay8486@gmail.com>
2022-01-27 17:09:57 -0300

    Requisito 3 refatorado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,10 @@ const data = require('../data/zoo_data');
 // Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
 
 // Nossa função primeiramente lida com caso de não preencimento do parametro e caso isso ocorra ela retorna um array vazio, caso contrario usamos o .find para encotrar e retornar o first ou lastName que bata com o passado no parametro.
-function getEmployeeByName(employeeName) {
+const getEmployeeByName = (employeeName) => {
   if (!employeeName) return {};
   return data.employees
     .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
-}
+};
 
 module.exports = getEmployeeByName;
