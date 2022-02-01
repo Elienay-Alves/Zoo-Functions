20cab673fdc139aacfa91ef3aec094dd8db06c71
Elienay-Alves <elienay8486@gmail.com>
2022-02-01 18:58:04 -0300

    Requisito 9 Feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeInfo = employees.find((employee) => employee.id === id);
+  const firstAnimalResponsible = data.species.find((specie) => specie.id === employeeInfo
+    .responsibleFor[0]);
+  const oldestResident = firstAnimalResponsible.residents
+    .sort((resident1, resident2) => resident2.age - resident1.age);
+  return [oldestResident[0].name, oldestResident[0].sex, oldestResident[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
